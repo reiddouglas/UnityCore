1d4e9240cb097c3848290f3ad80ba6da6a7e65cb
Reid W <reiddwilliamson@gmail.com>
2026-03-14 23:27:54 -0700

    Added more event primitives

---
+++ Events/PrimitiveTypes/Bool/BoolEventListener.cs
@@ -0,0 +1,6 @@
+﻿namespace Assets.Scripts.EventChannels.PrimitiveTypes.Bool
+{
+    class BoolEventListener : BaseEventListener<bool>
+    {
+    }
+}
---
+++ Events/PrimitiveTypes/Float/FloatEventChannel.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(
+    fileName = "FloatEventChannel",
+    menuName = "Events/Float Event Channel"
+)]
+public class BoolEventChannel : BaseEventChannel<float>
+{
+}
