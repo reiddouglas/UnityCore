6fae309a4b74681430a13acbbdcc36890147b6f9
Reid W <reiddwilliamson@gmail.com>
2026-04-11 17:26:29 -0700

    removed duplicate namespace in BoolEventListener

--- Assets/Scripts/Core/Events/Primitives/BoolEventListener.cs
+++ Assets/Scripts/Core/Events/Primitives/BoolEventListener.cs
@@ -1,6 +1,6 @@
 ﻿using Assets.Scripts.Core.Events.Base;
 
-namespace namespace Assets.Scripts.Core.Events.Primitives
+namespace Assets.Scripts.Core.Events.Primitives
 {
     class BoolEventListener : BaseEventListener<bool>
     {
